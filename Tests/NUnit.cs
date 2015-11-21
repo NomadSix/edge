aef811699ace5d37f36d37f5bd3b1e9009b0296c
Addie Morrison <caffinatedangel795@gmail.com>
2015-11-20 19:51:54 -0600

    Testing Support

---
+++ Tests/NUnit.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Edge.Tests {
+	[TestFixture]
+	public class NUnit {
+		[Test]
+		public void BasicAssert() {
+			Assert.True(true);
+		}
+	}
+}
+
